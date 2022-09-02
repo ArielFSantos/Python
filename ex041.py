e32b80c9a170e5f3c005896493687dd8308f1266
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2022-09-02 20:05:04 -0300

    att estudo

--- ex041.py
+++ ex041.py
@@ -1,6 +1,13 @@
-ano = int(input('Informe o Ano de Nascimento'))
+ano = int(input('Informe o Ano de Nascimento : '))
 idade = 2022-ano
 
-if idade < 10
-    print('Voce tem {} Anos. Categoria Mirm'.format(idade))
-if idade > 10 and idade < 15
+if idade < 10:
+    print('Voce tem {} Anos. Categoria Mirim'.format(idade))
+elif idade > 9 and idade < 15:
+    print('Voce tem {} Anos. Categoria Infantil'.format(idade))
+elif idade > 14 and idade < 20:
+    print('Voce tem {} Anos. Categoria Junior'.format(idade))
+elif idade > 19 and idade < 26:
+    print('Voce tem {} Anos. Categoria Senior'.format(idade))
+else:
+    print('Voce tem {} Anos. Categoria Master'.format(idade))
