98601df65e3938e94abe4ffa4c3ffdf92c6166ec
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2022-10-12 21:02:22 -0300

    att estudo

--- conjuntoOFC.py
+++ conjuntoOFC.py
@@ -1,17 +1,19 @@
 listaNum = []
+
 def group1(choice):
     return group2([],choice)
 
-def group2(current, group):
+def group2(current,group):
     if group:
         return group2(current,group[1:]) + group2(current+[group[0]],group[1:])
     return [current]
 
 while True:
-    num= int(input('Digite um Numero [0 Para Sair]: '))
+    num = int(input('Digite um Numero [0 Para Sair]: '))
     if num == 0 :
         break
     listaNum.append(num)
 
 choice = listaNum
 print(group1(choice))
+
