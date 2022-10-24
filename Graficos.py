694886c5f7b680b4e7bf07dfc3e1dc2147581cc4
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2022-10-24 12:58:38 -0300

    att

--- Graficos.py
+++ Graficos.py
@@ -1,13 +1,25 @@
 import matplotlib.pyplot as plt
 
-x = [1,2,3,4,5]
-y = [100,0,0,150,0]
-b = [0,40,50,0,45]
-c = [0,0,100,300,50]
+dias = []
+vest = []
+transp = []
+aliment = []
 
-plt.plot(x,y,label='Alimentação',marker='o',markerfacecolor = 'Blue')
-plt.plot(x,b,label='Vestuario',marker='o',markerfacecolor = 'Blue')
-plt.plot(x,c,label='Transporte',marker='o',markerfacecolor = 'Blue')
+quantdias = int(input('Quantos dias: '))
+
+for c in range(1,quantdias+1):
+
+    dias.append(c)
+    aux = int(input(f'Gasto dia {c} Com Alimentação: '))
+    aliment.append(aux)
+    aux = int(input(f'Gasto dia {c} Com Vestuario: '))
+    vest.append(aux)
+    aux = int(input(f'Gasto dia {c} Com Transporte: '))
+    transp.append(aux)
+
+plt.plot(dias,aliment,label='Alimentação',marker='o',markerfacecolor = 'Blue')
+plt.plot(dias,vest,label='Vestuario',marker='o',markerfacecolor = 'Blue')
+plt.plot(dias,transp,label='Transporte',marker='o',markerfacecolor = 'Blue')
 
 plt.title('Gráficos de Despesas')
 plt.ylabel('Despesas em R$')
