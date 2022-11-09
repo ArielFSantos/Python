fdb85454b29798621ea0565ee7250662206da0dc
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2022-11-09 20:11:41 -0300

    att trabalho

--- Graficos.py
+++ Graficos.py
@@ -8,30 +8,27 @@ transp = []
 aliment = []
 
 class Graficos:
-    def __init__(self,despesa,nome):
+    def __init__(self,despesa,nome,dias):
         self.despesa = despesa
         self.nome = nome
-
-    def Regressao(self):
-        x = np.array(dias).reshape(-1, 1)
+        self.dias = dias
+    def GerarGrafico(self):
+        x = np.array(self.dias).reshape(-1, 1)
         reg = LinearRegression().fit(x, self.despesa)
         a_coeff = reg.coef_
         l_coeff = reg.intercept_
-        plt.plot(dias, l_coeff + a_coeff * dias, label='Regressão', color='red')
-        Graficos.GerarGrafico(self)
-
-    def GerarGrafico(self):
-        plt.plot(dias, self.despesa, label=f'{self.nome}', marker='o', markerfacecolor='Blue')
+        plt.plot(self.dias, l_coeff + a_coeff * self.dias, label='Regressão', color='red')
+        plt.plot(self.dias, self.despesa, label=f'{self.nome}', marker='o', markerfacecolor='Blue')
         plt.title(f'Despesas de {self.nome}')
         plt.ylabel('Despesas em R$')
         plt.xlabel('Dia')
         plt.legend(loc=2)
-        plt.show
-    @staticmethod
-    def GraficoGeral():
-        plt.plot(dias, aliment, label='Alimentação', marker='o')
-        plt.plot(dias, vest, label='Vestimenta', marker='o')
-        plt.plot(dias, transp, label='Transporte', marker='o')
+        plt.show()
+
+    def GraficoGeral(self):
+        plt.plot(self.dias, aliment, label='Alimentação', marker='o')
+        plt.plot(self.dias, vest, label='Vestimenta', marker='o')
+        plt.plot(self.dias, transp, label='Transporte', marker='o')
         plt.title('Gráfico de Despesas')
         plt.ylabel('Despesa em R$')
         plt.xlabel('Dia')
@@ -52,13 +49,15 @@ while True:
         break
 
 sorted(dias)
-despesa1 = Graficos(vest,'Vestimenta')
-despesa2 = Graficos(transp,'Transporte')
-despesa3 = Graficos(aliment,'Alimentação')
-despesa1.Regressao()
-despesa2.Regressao()
-despesa3.Regressao()
-Graficos.GraficoGeral()
+despesa1 = Graficos(vest,'Vestimenta',dias)
+despesa2 = Graficos(transp,'Transporte',dias)
+despesa3 = Graficos(aliment,'Alimentação',dias)
+despesa1.GerarGrafico()
+despesa2.GerarGrafico()
+despesa3.GerarGrafico()
+
+
+despesa1.GraficoGeral()
 
 
 
--- textAux2.py
+++ textAux2.py
@@ -2,23 +2,23 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-dias = []
-vest = []
-transp = []
-aliment = []
+dias = [1,2,3,4,5]
+vest = [4,5,8,6,10]
+transp = [6,2,3,8,9]
+aliment = [2,4,6,1,2]
 
 class Graficos:
-    def __init__(self,despesa,nome):
+    def __init__(self,despesa,nome,dias):
         self.despesa = despesa
         self.nome = nome
-
+        self.dias = nome
     def Regressao(self):
-        x = np.array(dias).reshape(-1, 1)
+        x = np.array(self.dias).reshape(-1, 1)
         reg = LinearRegression().fit(x, self.despesa)
         a_coeff = reg.coef_
         l_coeff = reg.intercept_
-        plt.plot(dias, l_coeff + a_coeff * dias, label='Regressão', color='red')
-        Graficos.GerarGrafico(self)
+        plt.plot(self.dias, l_coeff + a_coeff * self.dias, label='Regressão', color='red')
+        plt.show()
 
     def GerarGrafico(self):
         plt.plot(dias, self.despesa, label=f'{self.nome}', marker='o', markerfacecolor='Blue')
@@ -26,42 +26,26 @@ class Graficos:
         plt.ylabel('Despesas em R$')
         plt.xlabel('Dia')
         plt.legend(loc=2)
-        plt.show
 
-    @staticmethod
-    def GraficoGeral():
-        plt.plot(dias, aliment, label='Alimentação', marker='o')
-        plt.plot(dias, vest, label='Vestimenta', marker='o')
-        plt.plot(dias, transp, label='Transporte', marker='o')
-        plt.title('Gráfico de Despesas')
-        plt.ylabel('Despesa em R$')
-        plt.xlabel('Dia')
-        plt.legend()
+    def GraficoGeral(self):
         plt.show()
 
-while True:
-    dias.append(int(input('Qual o dia? ')))
-    aliment.append(float(input('Valor Gasto em Alimentação: R$')))
-    vest.append(float(input('Valor Gasto em Vestuário: R$')))
-    transp.append(float(input('Valor Gasto em Transporte: R$')))
-    while True:
-        choice = str(input('Deseja? continuar? [S/N] ')).strip().upper()[0]
-        if choice in 'SN':
-            break
-        print('ERRO! Responda apenas S ou N.')
-    if choice == 'N':
-        break
 
-sorted(dias)
+
+
+
+
 despesa1 = Graficos(vest,'Vestimenta')
 despesa2 = Graficos(transp,'Transporte')
 despesa3 = Graficos(aliment,'Alimentação')
+
 despesa1.GerarGrafico()
 despesa2.GerarGrafico()
 despesa3.GerarGrafico()
-Graficos.GraficoGeral()
 
+Graficos.GraficoGeral(dias)
 
-
-
+despesa1.GerarGrafico()
+despesa2.GerarGrafico()
+despesa3.GerarGrafico()
 
