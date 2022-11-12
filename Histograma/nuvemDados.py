33df23e24d11c4be9a82cb4b8da9e3ce8fd326f6
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2022-11-12 00:23:49 -0300

    att

--- Histograma/nuvemDados.py
+++ Histograma/nuvemDados.py
@@ -7,19 +7,17 @@ pont = []
 fk = Faker()
 
 with open("dados.txt",'w') as arquivo:
-    for c in range(0, 100):
-        arquivo.write(fk.name() + str(randint(0,11)) +'\n')
-
-
+    for c in range(0, 50):
+        arquivo.write(fk.name() + str(randint(1,11)) +'\n')
 with open("dados.txt",'r') as arquivo:
     for v in arquivo:
         nomes.append(v[:-2])
         pont.append(int(v[-2]))
 
-
-plt.hist(pont, label='Histograma das Pontuações')
-plt.ylabel('Pontuações')
-plt.xlabel('Probabilidades')
-plt.grid('5')
-
+plt.hist(pont)
+plt.title('Histograma das Pontuações')
+plt.ylabel('Probabilidade')
+plt.xlabel('Pontuações')
+plt.grid("5")
 plt.show()
+ 
