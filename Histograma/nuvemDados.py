0fbeb06f9d48cbd39e9b2b6f75c5c148545075c2
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2022-11-14 18:47:18 -0300

    att

--- Histograma/nuvemDados.py
+++ Histograma/nuvemDados.py
@@ -1,11 +1,31 @@
 from matplotlib import pyplot as plt
-from faker import Faker
+from wordcloud import WordCloud
+from num2words import num2words
 from random import randint
+from faker import Faker
 
 nomes = []
 pont = []
-fk = Faker()
+namePont = []
+def Histograma():
+    plt.hist(pont,density=True)
+    plt.title('Histograma das Pontuações')
+    plt.ylabel('Probabilidade')
+    plt.xlabel('Pontuações')
+    plt.grid("5")
+    plt.show()
+def wc():
+    for c in pont:
+        namePont.append(num2words(c, lang='pt-br').title())
+    itens = " ".join(p for p in namePont)
+    wordcloud = WordCloud().generate(itens)
+    wordcloud.to_file("WorldCloud.jpg")
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.show()
 
+
+fk = Faker()
 with open("dados.txt",'w') as arquivo:
     for c in range(0, 50):
         arquivo.write(fk.name() + str(randint(1,11)) +'\n')
@@ -14,9 +34,6 @@ with open("dados.txt",'r') as arquivo:
         nomes.append(v[:-2])
         pont.append(int(v[-2]))
 
-plt.hist(pont)
-plt.title('Histograma das Pontuações')
-plt.ylabel('Probabilidade')
-plt.xlabel('Pontuações')
-plt.grid("5")
-plt.show()
+Histograma()
+wc()
+
--- Histograma/teste.py
+++ Histograma/teste.py
@@ -1,26 +1,39 @@
 from matplotlib import pyplot as plt
-from faker import Faker
+from wordcloud import WordCloud
+from num2words import num2words
 from random import randint
+from faker import Faker
 
 nomes = []
 pont = []
+namePont = []
+def Histograma():
+    plt.hist(pont)
+    plt.title('Histograma das Pontuações')
+    plt.ylabel('Probabilidade')
+    plt.xlabel('Pontuações')
+    plt.grid("5")
+    plt.show()
+def wc():
+    for c in pont:
+        namePont.append(num2words(c, lang='pt-br').title())
+    itens = " ".join(p for p in namePont)
+    wordcloud = WordCloud().generate(itens)
+    wordcloud.to_file("WorldCloud.jpg")
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.show()
+
+
 fk = Faker()
-
 with open("dados.txt",'w') as arquivo:
-    for c in range(0, 100):
-        arquivo.write(fk.name() + str(randint(0,11)) +'\n')
-
-
+    for c in range(0, 50):
+        arquivo.write(fk.name() + str(randint(1,11)) +'\n')
 with open("dados.txt",'r') as arquivo:
     for v in arquivo:
         nomes.append(v[:-2])
         pont.append(int(v[-2]))
 
-
-plt.hist(pont, label='Histograma das Pontuações')
-plt.ylabel('Pontuações')
-plt.xlabel('Probabilidades')
-plt.grid('5')
-
-plt.show()
+Histograma()
+wc()
 
