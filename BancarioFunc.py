23d0759763d080359e5835732d5c1e11fd92e477
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2022-12-25 09:02:46 -0300

    melhoria codigo bancario

--- BancarioFunc.py
+++ BancarioFunc.py
@@ -1,42 +1,46 @@
-saldo=1000
+saldo = 1000
 limite_saque = 3
 extrato = ""
 usuarios = []
+contas = []
+numero_conta = 0
 
-def op_saque(saldo,limite_saque,extrato):
+
+def op_saque():
+    global saldo
+    global limite_saque
+    global extrato
+
     if limite_saque != 0:
         limite_saque -= 1
         value = int(input('Qual Valor Deseja Sacar?: R$ '))
         while value > 500 or value < 1:
             print(20 * '=-')
             value = int(input('Valor Invalido!" \nQual Valor Deseja Sacar?: R$ '))
-        if value > 0 and value < 501:
-            if saldo > value:
-                extrato += f'\nSaque: R${value}'
-                saldo -= value
-            while value > saldo:
-                print(20 * '=-')
-                value = int(input('Valor Muito Alto, Digite Valor menor: R$ '))
-                saldo -= value
-
+        while value > saldo:
+            print(20 * '=-')
+            value = int(input('Valor Muito Alto, Digite Valor menor: R$ '))
+        saldo -= value
+        extrato += f'\nSaque: R${value}'
     else:
         print('Limite de Saque Excedido')
 
 
-def op_deposito(extrato,saldo,/):
+def op_deposito():
+    global extrato
+    global saldo
     value = int(input('Digite o Valor do Deposito: R$ '))
-    extrato += f'\nDeposito: R${value}'
     while value < 1:
         print('Valor Invalido!"')
         value = int(input('Digite o Valor do Deposito: R$ '))
     saldo += value
+    extrato += f'\nDeposito: R${value}'
 
 
-def op_extrato():
+def op_extrato(extrato, saldo):
     print('-=-=-=-=-=-= EXTRATO =-=-=-=-=-=-')
     print(extrato)
-    print('Saldo Atual da Conta: R$ ', saldo)
-
+    print('Saldo Atual da Conta: R$', saldo)
 
 
 def op_novo_usuario(usuarios):
@@ -45,48 +49,103 @@ def op_novo_usuario(usuarios):
     list_aux.append(str(input('Digite o Seu Endereço: ')))
     aux = int(input('Digite Seu CPF: '))
     for cpf in usuarios:
-        while cpf == aux:
+        while cpf[2] == aux:
             print('O CPF ja esta em uso')
             aux = int(input('Digite Seu CPF: '))
     list_aux.append(aux)
     usuarios.append(list_aux)
 
-while True:
-    print(20 * '=-')
-    print('[1] Saque '
-          '\n[2] Deposito '
-          '\n[3] Extrato '
-          '\n[4] Nova Conta '
-          '\n[5] Listar Contas'
-          '\n[6] Novo Usuário'
-          '\n[7] Sair ')
-    choice = int(input("Qual Operação Deseja Realizar?: "))
-    print(20 * '=-')
-    while choice < 1 or choice > 7:
+
+def op_nova_conta(contas):
+    global numero_conta
+    numero_conta += 1
+    list_aux = []
+    list_aux.append('001')
+    list_aux.append(numero_conta)
+    cont = 0
+    for user in usuarios:
+        cont += 1
+        print(cont, user[0])
+    aux = int(input('Selecione o usuario: '))
+    aux = usuarios[aux - 1][0]
+    list_aux.append(aux)
+    contas.append(list_aux)
+    print(contas)
+
+
+def op_lista(contas, usuarios):
+    choice = int(input('[1] Listar Contas'
+                       '\n[2] Listar Usuarios '
+                       '\nOperação: '))
+
+    while choice != 1 and choice != 2:
         print('Escolha Invalida!"')
+        choice = int(input('[1] Listar Contas'
+                           '\n[2] Listar Usuarios'
+                           '\nOperação: '))
+    if choice == 1:
+        for cont in contas:
+            print(cont)
+    else:
+        for user in usuarios:
+            print(user)
+
+
+def op_saida():
+    if extrato == "":
+        print('Não Foram Realizadas Movimentações')
+    print('Saldo Atual da Conta: R$ ', saldo)
+    print('Obrigado, Tenha um Bom dia <3')
+
+
+def main():
+    while True:
+        print(20 * '=-')
         print('[1] Saque '
               '\n[2] Deposito '
               '\n[3] Extrato '
-              '\n[4] Nova Conta '
-              '\n[5] Listar Contas'
-              '\n[6] Novo Usuário'
-              '\n[7] Sair ')
+              '\n[4] Adicionar '
+              '\n[5] Listar'
+              '\n[0] Sair ')
         choice = int(input("Qual Operação Deseja Realizar?: "))
         print(20 * '=-')
-    if choice == 1:
-        op_saque(saldo=saldo,limite_saque=limite_saque,extrato=extrato)
-    elif choice == 2:
-        op_deposito(extrato,saldo)
-    elif choice == 3:
-        op_extrato()
+        while choice < 0 or choice > 7:
+            print('Escolha Invalida!"')
+            print('[1] Saque '
+                  '\n[2] Deposito '
+                  '\n[3] Extrato '
+                  '\n[4] Adicionar '
+                  '\n[5] Listar'
+                  '\n[0] Sair ')
+            choice = int(input("Qual Operação Deseja Realizar?: "))
+            print(20 * '=-')
 
-    elif choice == 6:
-        op_novo_usuario(usuarios)
-    elif choice == 7:
-        if extrato == "":
-            print('Não Foram Realizadas Movimentações')
-        print('Saldo Atual da Conta: R$ ', saldo)
-        print(usuarios)
-        print('Obrigado, Tenha um Bom dia <3')
-        break
+        if choice == 1:
+            op_saque()
+        elif choice == 2:
+            op_deposito()
+        elif choice == 3:
+            op_extrato(extrato, saldo)
+        elif choice == 4:
+            while True:
+                choice = int(input('[1] Nova Conta '
+                                   '\n[2] Novo Usuario'
+                                   '\n[0] Sair'
+                                   '\nOperação : '))
+                if choice == 2:
+                    op_novo_usuario(usuarios)
+                if choice == 1:
+                    op_nova_conta(contas)
+                if choice == 0:
+                    break
+        elif choice == 5:
+            op_lista(contas, usuarios)
 
+        elif choice == 6:
+            op_novo_usuario(usuarios)
+        elif choice == 0:
+            op_saida()
+            break
+
+
+main()
--- texteAux.py
+++ texteAux.py
@@ -1,72 +1,146 @@
-import numpy as np
-from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
-
-dias = [1,2,3,4,5]
-vest = [4,5,8,6,10]
-transp = [6,2,3,8,9]
-aliment = [2,4,6,1,2]
-
-class Graficos:
-    def __init__(self,despesa,nome):
-        self.despesa = despesa
-        self.nome = nome
-
-    def Regressao(self):
-        x = np.array(dias).reshape(-1, 1)
-        reg = LinearRegression().fit(x, self.despesa)
-        a_coeff = reg.coef_
-        l_coeff = reg.intercept_
-        plt.plot(dias, l_coeff + a_coeff * dias, label='Regressão', color='red')
-        Graficos.GerarGrafico(self)
-    def GerarGrafico(self):
-
-        plt.plot(dias, self.despesa, label=f'{self.nome}', marker='o',markerfacecolor='Blue')
-        plt.title(f'Despesas de {self.nome}')
-        plt.ylabel('Despesas em R$')
-        plt.xlabel('Dia')
-        plt.legend(loc=2)
-        plt.show()
-    def GraficoGeral(self):
-        plt.plot(dias, aliment, label='Alimentação', marker='o')
-        plt.plot(dias, vest, label='Vestimenta', marker='o')
-        plt.plot(dias, transp, label='Transporte', marker='o')
-        plt.title('Gráfico de Despesas')
-        plt.ylabel('Despesa em R$')
-        plt.xlabel('Dia')
-        plt.legend()
-        plt.show()
+saldo = 1000
+limite_saque = 3
+extrato = ""
+usuarios = []
+contas = []
+numero_conta = 0
 
 
-despesa1 = Graficos(vest,'Vestimenta')
-despesa2 = Graficos(transp,'Transporte')
-despesa3 = Graficos(aliment,'Alimentação')
-despesa1.Regressao()
+def op_saque(saldo,limite_saque,extrato):
+
+    if limite_saque != 0:
+        limite_saque -= 1
+        value = int(input('Qual Valor Deseja Sacar?: R$ '))
+        while value > 500 or value < 1:
+            print(20 * '=-')
+            value = int(input('Valor Invalido!" \nQual Valor Deseja Sacar?: R$ '))
+        while value > saldo:
+            print(20 * '=-')
+            value = int(input('Valor Muito Alto, Digite Valor menor: R$ '))
+        saldo -= value
+        extrato += f'\nSaque: R${value}'
+    else:
+        print('Limite de Saque Excedido')
+    return saldo,limite_saque,extrato
+
+def op_deposito(extrato,saldo):
+    value = int(input('Digite o Valor do Deposito: R$ '))
+    while value < 1:
+        print('Valor Invalido!"')
+        value = int(input('Digite o Valor do Deposito: R$ '))
+    saldo += value
+    extrato += f'\nDeposito: R${value}'
+    return extrato,saldo
+
+
+def op_extrato(extrato, saldo):
+    print('-=-=-=-=-=-= EXTRATO =-=-=-=-=-=-')
+    print(extrato)
+    print('Saldo Atual da Conta: R$', saldo)
+
+
+def op_novo_usuario(usuarios):
+    list_aux = []
+    list_aux.append(str(input('Digite o Seu Nome: ')))
+    list_aux.append(str(input('Digite o Seu Endereço: ')))
+    aux = int(input('Digite Seu CPF: '))
+    for cpf in usuarios:
+        while cpf[2] == aux:
+            print('O CPF ja esta em uso')
+            aux = int(input('Digite Seu CPF: '))
+    list_aux.append(aux)
+    usuarios.append(list_aux)
+
+
+def op_nova_conta(contas,numero_conta):
+    numero_conta += 1
+    list_aux = []
+    list_aux.append('001')
+    list_aux.append(numero_conta)
+    cont = 0
+    for user in usuarios:
+        cont += 1
+        print(cont, user[0])
+    aux = int(input('Selecione o usuario: '))
+    aux = usuarios[aux - 1][0]
+    list_aux.append(aux)
+    contas.append(list_aux)
+    print(contas)
+    return numero_conta
+
+
+def op_lista(contas, usuarios):
+    choice = int(input('[1] Listar Contas'
+                       '\n[2] Listar Usuarios '
+                       '\nOperação: '))
+
+    while choice != 1 and choice != 2:
+        print('Escolha Invalida!"')
+        choice = int(input('[1] Listar Contas'
+                           '\n[2] Listar Usuarios'
+                           '\nOperação: '))
+    if choice == 1:
+        for cont in contas:
+            print(cont)
+    else:
+        for user in usuarios:
+            print(user)
+
+
+def op_saida():
+    if extrato == "":
+        print('Não Foram Realizadas Movimentações')
+    print('Saldo Atual da Conta: R$ ', saldo)
+    print('Obrigado, Tenha um Bom dia <3')
 
 
 
+while True:
+    print(20 * '=-')
+    print('[1] Saque '
+          '\n[2] Deposito '
+          '\n[3] Extrato '
+          '\n[4] Adicionar '
+          '\n[5] Listar'
+          '\n[0] Sair ')
+    choice = int(input("Qual Operação Deseja Realizar?: "))
+    print(20 * '=-')
+    while choice < 0 or choice > 7:
+        print('Escolha Invalida!"')
+        print('[1] Saque '
+              '\n[2] Deposito '
+              '\n[3] Extrato '
+              '\n[4] Adicionar '
+              '\n[5] Listar'
+              '\n[0] Sair ')
+        choice = int(input("Qual Operação Deseja Realizar?: "))
+        print(20 * '=-')
 
+    if choice == 1:
+        saldo, limite_saque, extrato = op_saque(saldo=saldo,limite_saque=limite_saque,extrato=extrato)
+    elif choice == 2:
+        extrato, saldo = op_deposito(extrato,saldo)
+    elif choice == 3:
+        op_extrato(extrato, saldo)
+    elif choice == 4:
+        while True:
+            choice = int(input('[1] Nova Conta '
+                               '\n[2] Novo Usuario'
+                               '\n[0] Sair'
+                               '\nOperação : '))
+            if choice == 2:
+                op_novo_usuario(usuarios)
+            if choice == 1:
+                op_nova_conta(contas)
+            if choice == 0:
+                break
+    elif choice == 5:
+        op_lista(contas, usuarios)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    elif choice == 6:
+        op_novo_usuario(usuarios)
+    elif choice == 0:
+        op_saida()
+        break
 
 
