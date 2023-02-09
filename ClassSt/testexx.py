7154fbd5a90466a69d5519fa684734f1d1d13295
ArielFSantos <94645030+ArielFSantos@users.noreply.github.com>
2023-02-08 21:22:33 -0300

    att estudo

--- ClassSt/testexx.py
+++ ClassSt/testexx.py
@@ -1,12 +1,23 @@
 class Quadrado():
-    def __init__(self,size1,size2):
-        self.recovery = None
-        self.recovery2 = None
+    def __init__(self, size1, size2):
         self.size1 = size1
         self.size2 = size2
+
     def Mudar(self):
-        self.recovery = self.size1
-        self.recovery2 = self.size2
+
+        choice = int(input('Lado 1 ou Lado 2: '))
+
+        if choice == 1:
+            value = int(input('Novo Valor: '))
+            self.size1 = value
+            self.recovery = self.size1
+        if choice == 2:
+            value = int(input('Novo Valor: '))
+            self.size2 = value
+            self.recovery = self.size2
+
+    def Retornar(self):
+
         choice = int(input('Lado 1 ou Lado 2: '))
 
         if choice == 1:
@@ -16,17 +27,15 @@ class Quadrado():
             value = int(input('Novo Valor: '))
             self.size2 = value
 
-    def Retornar(self):
-        self.size1 = self.recovery
-        self.size2 = self.recovery2
+    def pergunta(self):
 
     def Calcular(self):
         area = self.size1 * self.size2
         print(f'O Valor da Area é : {area}')
 
-quad = Quadrado(size1=3,size2=5)
+
+quad = Quadrado(size1=5, size2=5)
 quad.Calcular()
 quad.Mudar()
 quad.Calcular()
-quad.Retornar()
-quad.Calcular()
+
